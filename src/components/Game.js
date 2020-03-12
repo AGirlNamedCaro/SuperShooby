a9d0409f874da3034b054d26628d8e3b05bba42c
Carolina Contreras Issa <agirlnamedcaro@gmail.com>
2020-03-12 14:47:56 -0600

    placed preload and create into their own files and imported them into the main Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Phaser from "phaser";
+import {preload, create} from './helpers';
 
-const config = {
+export const config = {
   type: Phaser.CANVAS,
   width: 800,
   height: 600,
@@ -17,38 +18,7 @@ const config = {
       create: create
   }
 };
-
 const game = new Phaser.Game(config);
-
-function preload ()
-{
-  this.load.setBaseURL('https://labs.phaser.io');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
-}
-
-function create ()
-{
-  this.add.image(400, 300, 'sky');
-
-  const particles = this.add.particles('red');
-
-  const emitter = particles.createEmitter({
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: 'ADD'
-  });
-
-  const logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
-}
 
 
 export default function Game() {
