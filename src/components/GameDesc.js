cdaca08ffbdd70890b08ffa53a7a8262db9bcd71
CliffGreene <lifeguardtwenty@gmail.com>
2020-03-12 18:56:13 -0600

    adding a few css stylings to main container

--- src/components/GameDesc.js
+++ src/components/GameDesc.js
@@ -4,7 +4,12 @@ import "./GameDesc.css";
 const GameDesc = () => {
   return (
     <article id="game-desc">
-      <h1>Game Desc goes here!</h1>
+      <h1>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
+        do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis a
+        ute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla paria
+        tur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+        anim id est laborum."</h1>
     </article>
   );
 };
