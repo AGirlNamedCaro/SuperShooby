1643ada573fc634503182a6b5df353a95aa7e647
CliffGreene <lifeguardtwenty@gmail.com>
2020-03-18 12:01:19 -0600

    Adding styling, and instructions

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,7 +4,8 @@ import "./TopBar.css"
 export default function TopBar() {
   return (
     <article id="top-bar" >
-    <img class='full-height' src="/assets/images/backgrounds/logo.png" alt=""/>
+    {/* <img class='full-height' src="/assets/images/backgrounds/logo.png" alt=""/> */}
   </article>
   );
 }
+
